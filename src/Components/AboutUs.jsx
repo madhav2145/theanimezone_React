3157eae116be5fff88c8d58adcfd27d268ef9bd1
madhur333 <madhurmadhav333@gmail.com>
2023-12-02 13:58:38 +0530

    changes to about us

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -1,64 +1,83 @@
-// AboutUs.js
-import React from 'react';
-
+import React from "react";
+import logo from "../assets/logo192.png";
+import amv from "../assets/Gang_Torture_Dance.mp4"
 const AboutUs = () => {
-  return (<div className="box-border h-screen w-screen bg-slate-500 relative p-5 space-x-10 space-y-10">
-  <div className="h-full w-1/2 bg-black relative">1</div>
-  <div className="h-1/2 w-1/2 bg-red-800 absolute top-0 right-0 m-5">2</div>
-  <div className="h-[40%] w-[48%] bg-yellow-500 absolute bottom-5 right-5 m-5">3</div>
-</div>
+  return (
+    <div className="box-border h-screen w-full bg-gradient-to-r from-gray-700 via-gray-900 to-black p-5 relative space-y-5">
+      <div className="h-full w-1/2 border border-red-600 rounded-xl bg-slate-300 relative">
+        <div className="h-full w-[full] rounded-xl bg-gradient-to-t from-red-700 via-slate-800 to-gray-900">
+          <h5 className="text-center text-7xl pt-[4rem] text-red-600 ">THEANIMEZONE</h5>
+          <p className="text-white">
+            Your go-to destination for discovering and tracking your favorite
+            anime series and movies.
+          </p>
+          <video src={amv} autoPlay loop muted className="h-[75%] w-[95%] pl-10" />
+        </div>
+      </div>
+      <div className="h-[46%] w-[48%] border border-red-600 rounded-xl bg-gradient-to-t from-red-700 via-slate-800 to-gray-900 absolute top-0 right-5 text-white ">
+        <h1 className="text-left ml-5 mt-5 mr-[60%] p-2 text-2xl border border-red-600 rounded-xl text-red-600 ">
+          Key Features
+        </h1>
+        <ul className="list-disc list-inside">
+          <li className="text-left p-2 pl-5">
+            Our website features a vast database of anime series and movies,
+            ensuring you can find information on almost any title you're
+            interested in.
+          </li>
+          <li className="text-left p-2 pl-5">
+            Share your thoughts on your favorite (or not-so-favorite) anime
+            titles. Read reviews from other users to discover hidden gems or
+            avoid disappointments.
+          </li>
+          <li className="text-left p-2 pl-5">
+            Create and manage your own anime lists – whether it's a "Plan to
+            Watch" list, a list of favorites, or a history of what you've
+            watched.
+          </li>
+          <li className="text-left p-2 pl-5">
+            Join discussions, forums, and interact with fellow anime fans. Our
+            community is a welcoming space for sharing recommendations,
+            theories, and more.
+          </li>
+        </ul>
+      </div>
+      <div className="h-[46%] w-[48%] border border-red-600 rounded-xl bg-gradient-to-t from-red-700 via-slate-800 to-gray-900 absolute bottom-5 right-5 ">
+        <h1 className="text-left ml-5 mt-5 mr-[60%] p-2 text-2xl rounded-xl text-red-600">
+          Team Members
+        </h1>
+        <div className="grid grid-cols-3 text-white">
+          <div className="border border-red-600 container">
+            <img
+              src={logo}
+              alt="Your Alt Text"
+              className="h-[12rem] w-[14rem]  pl-6 pt-3 "
+            />
+            <h1>madhur</h1>
+            <p>iiitl student</p>
+          </div>
+          <div className="border border-red-600">
+            <img
+              src={logo}
+              alt="Your Alt Text"
+              className="h-[12rem] w-[14rem]  pl-6 pt-3 "
+            />
+            <h1>madhur</h1>
+            <p>iiitl student</p>
+          </div>
+          <div className="border border-red-600">
+            <img
+              src={logo}
+              alt="Your Alt Text"
+              className="h-[12rem] w-[14rem]  pl-6 pt-3 "
+            />
+            <h1>madhur</h1>
+            <p>iiitl student</p>
+          </div>
+        </div>
+      </div>
+    </div>
 
-
-    // <div className="box-border h-screen w-screen  bg-slate-500 relative p-5 space-x-10 space-y-10">
-    //   <div className="h-full w-1/2 bg-black static">1</div>
-    //   <div className="h-1/2 w-1/2 bg-red-800 absolute top-0 right-0 m-5">2</div>
-    //   <div className="h-1/2 w-1/2 bg-yellow-500 absolute bottom-0 right-0 m-5">3</div>
-    // </div>
-    // <div className="flex justify-center h-full w-full p-10 items-center">
-    //   <div className="p-5 border rounded-xl shadow-2xl shadow-indigo-500/50  text-black">
-    //     <h2 className="text-2xl font-bold  border-t-4 border-r-4 border-b-2 border-l-2  rounded-lg w-1/4 p-1  bg-black-500 text-black">About Us</h2>
-    //     <p>
-    //       Welcome to [Your Anime List Website], your go-to destination for discovering and tracking your favorite anime series and movies. Whether you're a seasoned otaku or just starting your anime journey, we've got you covered.
-    //     </p>
-
-    //     <h3 className=" border-t-4 border-r-4 border-b-2 border-l-2 rounded-lg w-1/5 p-1 text-xl font-bold mt-4  bg-black-500 text-black">Our Mission</h3>
-    //     <p>
-    //       At [Your Anime List Website], we are passionate about creating a comprehensive and user-friendly platform for anime enthusiasts. Our mission is to provide a space where you can explore, rate, and review anime titles, as well as connect with other like-minded fans.
-    //     </p>
-
-    //     <h3 className="border-t-4 border-r-4 border-b-2 border-l-2 rounded-lg w-1/5 p-1 text-xl font-bold mt-4  bg-black-500 text-black">Key Features</h3>
-    //     <ul className="list-disc pl-6">
-    //       <li><strong>Extensive Database:</strong> Our website features a vast database of anime series and movies, ensuring you can find information on almost any title you're interested in.</li>
-    //       <li><strong>User Reviews and Ratings:</strong> Share your thoughts on your favorite (or not-so-favorite) anime titles. Read reviews from other users to discover hidden gems or avoid disappointments.</li>
-    //       <li><strong>Personalized Lists:</strong> Create and manage your own anime lists – whether it's a "Plan to Watch" list, a list of favorites, or a history of what you've watched.</li>
-    //       <li><strong>Community Engagement:</strong> Join discussions, forums, and interact with fellow anime fans. Our community is a welcoming space for sharing recommendations, theories, and more.</li>
-    //     </ul>
-
-    //     <h3 className=" border-t-4 border-r-4 border-b-2 border-l-2 rounded-lg w-1/5 p-1 text-xl font-bold mt-4  bg-black-500 text-black">Meet the Team</h3>
-    //     <p>
-    //       The [Your Anime List Website] team is a group of dedicated anime enthusiasts who share a common goal – to make your anime-watching experience more enjoyable. We are committed to continuously improving and expanding our platform to meet the needs of the anime community.
-    //     </p>
-
-    //     <div className="flex justify-around mt-4">
-    //       {/* Add information about each team member */}
-    //       <div className="text-center">
-    //         <img src="member1.jpg" alt="Team Member 1" className="w-20 h-20 rounded-full mb-2 mx-auto" />
-    //         <h4 className="font-bold">John Doe</h4>
-    //         <p>Co-founder & Developer</p>
-    //       </div>
-    //       <div className="text-center">
-    //         <img src="member2.jpg" alt="Team Member 2" className="w-20 h-20 rounded-full mb-2 mx-auto" />
-    //         <h4 className="font-bold">Jane Smith</h4>
-    //         <p>Designer & UX Specialist</p>
-    //       </div>
-    //       {/* Add more team members as needed */}
-    //     </div>
-
-    //     <p className="mt-4">
-    //       Thank you for being a part of the [Your Anime List Website] community. We hope you enjoy exploring and discovering new anime titles with us!
-    //     </p>
-    //   </div>
-    // </div>
+    
   );
 };
 
